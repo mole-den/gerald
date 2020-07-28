3993f3948d1d28f2c40a46b424ceb62d71980b90
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-07-28 20:41:54 +1000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const emerald = "555477";
 const ice = "e";
 const date = "23/6/2020"
 
-bot.on('ready', () => {
+/*bot.on('ready', () => {
     bot.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
     bot.user.setPresence({
         game: {
@@ -14,7 +14,7 @@ bot.on('ready', () => {
             type: 2
         }
     });
-});   
+});   */
 
 
 const token = 'NjcxMTU2MTMwNDgzMDExNjA1.Xs9tTw.QOJZky89ROAnBWYiu1l9EDhk8q4';
@@ -88,9 +88,9 @@ function checkCommand(msg){
             }
         }
     } catch (e){
-        msg.channel.sendMessage('Error!');
-        msg.channel.sendMessage('Error code: ' + Math.floor(Math.random() * 999 + 1));
-        msg.channel.sendMessage(e);
+        channel.send('Error!');
+        channel.send('Error code: ' + Math.floor(Math.random() * 999 + 1));
+        channel.send(e);
         console.log('ERROR')
     }
     
@@ -98,7 +98,7 @@ function checkCommand(msg){
 
 //-----------------------Custom functions here------------------------------------
 function randomnumber(msg){
-    msg.channel.sendMessage(Math.floor(Math.random() * 10 + 1));
+    channel.send(Math.floor(Math.random() * 10 + 1));
 }
 
 
@@ -106,53 +106,53 @@ function randomnumber(msg){
 bot.on('message', msg=>{
     switch(msg.content){
         case wikiPrefix + "Harvester":
-            msg.reply('gwiki: Harvester Upgrades'); 
-            msg.channel.sendMessage('Once a great spud farmer decided he was tired of farming spuds by hand so he invented a Harvester that would do it for him.');
-            msg.channel.sendMessage('(Harvester I: 100 Spudcoin | 100 Spudcoin per hour) -> (Harvester II: 1000 Spudcoin | 200 Spudcoin per hour) -> (Harvester III: 10k Spudcoin | 300 Spudcoin per hour) -> (Harvester IV: 100k Spudcoin | 400 Spudcoin per hour) -> (Harvester V: 500k Spudcoin | 500 Spudcoin per hour) -> (Harvester VI: 1M Spudcoin | 1k Spudcoin per hour to bank) -> (Harvester VII: Buy from the Strange Auction | ???)');
+            channel.send('gwiki: Harvester Upgrades'); 
+            channel.send('Once a great spud farmer decided he was tired of farming spuds by hand so he invented a Harvester that would do it for him.');
+            channel.send('(Harvester I: 100 Spudcoin | 100 Spudcoin per hour) -> (Harvester II: 1000 Spudcoin | 200 Spudcoin per hour) -> (Harvester III: 10k Spudcoin | 300 Spudcoin per hour) -> (Harvester IV: 100k Spudcoin | 400 Spudcoin per hour) -> (Harvester V: 500k Spudcoin | 500 Spudcoin per hour) -> (Harvester VI: 1M Spudcoin | 1k Spudcoin per hour to bank) -> (Harvester VII: Buy from the Strange Auction | ???)');
             break;
         case wikiPrefix + "Mastery":
-            msg.reply('gwiki: Mastery Upgrades (outdated)'); 
-            msg.channel.sendMessage('Once, one farmer decended so far into potato madness he crossed the threshold of potatification. He then brought back these upgrades.');
-            msg.channel.sendMessage('(Mastery I: From Giveaways | 10k per hour) -> (Mastery II: 5M | 50k per hour) -> (Mastery III: 100M | 1M per hour to bank) -> (Mastery IV: 1000000000 | 10M per hour to bank)');
-            msg.channel.sendMessage('lol this is outdated');
+            channel.send('gwiki: Mastery Upgrades (outdated)'); 
+            channel.send('Once, one farmer decended so far into potato madness he crossed the threshold of potatification. He then brought back these upgrades.');
+            channel.send('(Mastery I: From Giveaways | 10k per hour) -> (Mastery II: 5M | 50k per hour) -> (Mastery III: 100M | 1M per hour to bank) -> (Mastery IV: 1000000000 | 10M per hour to bank)');
+            channel.send('lol this is outdated');
             break;
          case wikiPrefix + "Valuable Spuds":
-            msg.reply('gwiki: Valuable Spuds (outdated)'); 
-            msg.channel.sendMessage('MUST. HAVE. SHINY. SPUDS.');
-            msg.channel.sendMessage('(Valuable Spuds I: 10M | 100k per hour) -> (Valuable Spuds II: 30M | 500k per hour) -> (Valuable Spuds III: 100M | 1M per hour)');
+            channel.send('gwiki: Valuable Spuds (outdated)'); 
+            channel.send('MUST. HAVE. SHINY. SPUDS.');
+            channel.send('(Valuable Spuds I: 10M | 100k per hour) -> (Valuable Spuds II: 30M | 500k per hour) -> (Valuable Spuds III: 100M | 1M per hour)');
             break;
          case wikiPrefix + "Spudcoin":
-            msg.reply('gwiki: Spudcoin'); 
-            msg.channel.sendMessage('The strange spud currency of Project Flicker.');
+            channel.send('gwiki: Spudcoin'); 
+            channel.send('The strange spud currency of Project Flicker.');
             break;
          case wikiPrefix + "Midas Spuds":
-            msg.reply('gwiki: Midas Spuds'); 
-            msg.channel.sendMessage('probably does nothing (Price: 1B)');
+            channel.send('gwiki: Midas Spuds'); 
+            channel.send('probably does nothing (Price: 1B)');
             break;
          case wikiPrefix + "God of Spuds":
-            msg.reply('gwiki: God of Spuds'); 
-            msg.channel.sendMessage('After maxing out every other upgrade, a farmer must take on a final task: to become a god of spuds.');
-            msg.channel.sendMessage('10B | 100M per hour to bank');
+            channel.send('gwiki: God of Spuds'); 
+            channel.send('After maxing out every other upgrade, a farmer must take on a final task: to become a god of spuds.');
+            channel.send('10B | 100M per hour to bank');
             break;
          case wikiPrefix + "Reboots":
-            msg.reply('gwiki: Server Reboots'); 
-            msg.channel.sendMessage('Every once in a while, the server undergoes a cosmetic change called a reboot. During a reboot, farmers may sacrifice all their spudcoin and gameplay roles (not Eternal Service) to get a Rebooted item. Farmers keep their inventory when they reboot.');
+            channel.send('gwiki: Server Reboots'); 
+            channel.send('Every once in a while, the server undergoes a cosmetic change called a reboot. During a reboot, farmers may sacrifice all their spudcoin and gameplay roles (not Eternal Service) to get a Rebooted item. Farmers keep their inventory when they reboot.');
             break;
          case wikiPrefix + "Luck":
-            msg.reply('gwiki: Luck'); 
-            msg.channel.sendMessage('amazing WIP');
-            msg.channel.sendMessage('u can get from sweden querst and shop and other stuff');
+            channel.send('gwiki: Luck'); 
+            channel.send('amazing WIP');
+            channel.send('u can get from sweden querst and shop and other stuff');
             break;
          case wikiPrefix + "Strange Auction":
-            msg.reply('gwiki: Strange Auction');
-            msg.channel.sendMessage("get lot monez to buy stuf");
+            channel.send('gwiki: Strange Auction');
+            channel.send("get lot monez to buy stuf");
 	 	    //commands are good
 	 case commandPrefix + "flicker":
-	    msg.channel.sendMessage('Yës Brøthër');
-	    channel.send(Pfembed);
-	    msg.channel.sendMessage('https://discord.gg/Y2EtATM');
+	    channel.send('Yës Brøthër');
+	    //channel.send(Pfembed);
+	    channel.send('https://discord.gg/Y2EtATM');
 	 case commandPrefix + "help":
-	    msg.channel.sendMessage('no');
+	    channel.send('no');
 	    break;
 	 case commandPrefix + "errortest":
 	    throw ":(";
@@ -163,7 +163,7 @@ bot.on('message', msg=>{
     }
 })
 
-const PFembed = new Discord.MessageEmbed()
+/*const PFembed = new Discord.MessageEmbed()
 	.setColor('#31ef01')
 	.setTitle('Project Flicker [test]')
 	.setURL('https://discord.js.org/')
@@ -179,7 +179,7 @@ const PFembed = new Discord.MessageEmbed()
 	.addField('Inline field title', 'Some value here', true)
 	.setImage('https://i.imgur.com/wSTFkRM.png')
 	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');*/
 
 
 
