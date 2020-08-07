774d91abbff99fe54f6627ed145775b6fa5104af
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-08-07 13:08:13 +1000

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ bot.on('ready', () =>{
 });  
 
 
-
-
 bot.login(token);
+
+//Server Firewall code below
+
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'joining-room');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}. Remeber to follow the rules and praise me every Wednesday.`);
+});
+
