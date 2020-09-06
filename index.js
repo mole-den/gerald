6a212a572efdb5bade1303134398a270a5e38e33
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-09-07 09:26:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -87,9 +87,6 @@ bot.on('message', message => {
 		//const channelID = guild.channels.cache.find(ch => ch.name === "labyrinth-" + labyrID);
 		
 		guild.channels.create("labyrinth-" + labyrID)
-		/*.then(createdChannel => { 
-			const id = createdChannel.id 
-		})*/
 		.then(channel => {
     		let category = guild.channels.cache.find(c => c.id == "749883316223737906" && c.type == "category");
 		if (!category) throw new Error("lol it no work nerd");
