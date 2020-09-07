1eb954537b7aa8493ddea3ccbf794301e954dc79
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-09-07 18:20:40 +1000

    Update index.js

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ bot.on('message', message => {
 	
 
 
-
+const when = Date.now();
 function heartbeat() {
-	console.log(`Heartbeat recived. Logged in as ${bot.user.tag}`);
+	console.log(`Heartbeat recived. Logged in as ${bot.user.tag}. It is now ${when}. `);
 }
 
 setInterval(heartbeat, 10000);
