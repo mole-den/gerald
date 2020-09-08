29d19b2e0afc2d139049584b9408763230dc93e5
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-09-08 16:44:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on('guildMemberAdd', member => {
 });
 
 bot.on('message', message => {
-	console.log(`${message.author.tag} said: ${message.content} /*in ${message.guild.name}*/`);
+	console.log(`${message.author.tag} said: ${message.content}`);
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	
 	const args = message.content.slice(prefix.length).trim().split(' ');
