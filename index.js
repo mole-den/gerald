72054a5f5e307283f73ba6f5536d58befbbfc79d
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-11-12 13:16:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const Discord = require('discord.js'); //hello there yes
 const bot = new Discord.Client();
 const prefix = "g";
 const fs = require('fs');
-
+const AntiSpam = require('discord-anti-spam');
+const antiSpam = new AntiSpam({
+    warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+    kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+    banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+    warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
+    kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
+    banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
+    maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+    exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
+    ignoreBots: true, // Ignore bot messages.
+    verbose: true, // Extended Logs from module.
+    ignoredUsers: [], // Array of User IDs that get ignored.
+    // And many more options... See the documentation.
+});
 
 //var doheartbeat = true
 
@@ -35,6 +52,8 @@ bot.on('ready', () =>{
     bot.emit('heartbeated');
 });  
 
+
+bot.on('message', (message) => antiSpam.message(message)); 
 
 bot.login(token);
 
