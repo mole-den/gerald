2471d4487eb91d1e6142e8863aaa36483fec4ca5
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2021-08-20 11:41:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ bot.on('message', message => {
 	const command = args.shift().toLowerCase();
 	
 	if (command === `help`) {
-		message.channel.send('Hello! I am Gerald. I will enable you to take control of your server by my rule >:) (invite link coming soonish)');
+		message.channel.send('Hello! I am Gerald. I will enable you to take control of your server by my rules >:) (invite link coming soonish)');
 	} else if (command === `detect`) {
 	// grab the "first" mentioned user from the message
 	// this will return a `User` object, just like `message.author`
