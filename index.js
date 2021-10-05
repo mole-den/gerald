f20ff011ebadea98463717bc92bca41b765a633a
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2021-10-06 08:31:21 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Client, Intents } = require('discord.js');
 
 const myIntents = new Intents();
-myIntents.add(Intents.FLAGS.GUILD, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES);
+myIntents.add(Intents.FLAGS.GUILD, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_PRESENCES
+);
+
 
 const bot = new Client({ intents: myIntents });
 
