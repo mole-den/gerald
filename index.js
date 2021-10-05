34c164213788bcd8494fc6f99f911540058af4d9
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2021-10-05 14:23:41 +1100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, Intents } = require('discord.js');
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES);
 
-const bot = new Client({ intents: allIntents });
+const bot = new Client({ intents: myIntents });
 
 
 const prefix = "g";
