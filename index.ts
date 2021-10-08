800b505a7a8bda4fc7a434e8172af5d3ce7dec00
ofoxsmith <77560533+ofoxsmith@users.noreply.github.com>
2021-10-08 20:18:52 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -114,7 +114,8 @@ bot.on('message', async (message: discord.Message) => {
 			let str = message.content;
 			let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
 			if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
-				message.channel.send('You do not have the required permissions')
+				message.channel.send('You do not have the required permissions');
+				return;
 			}
 			if (dbConnected === true) {
 				console.log(out)
