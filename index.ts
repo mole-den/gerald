5e3413cfcb1d05243eda4b8caa8bb6eead510fe7
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-10-09 12:07:06 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -133,10 +133,10 @@ bot.on('message', async (message: discord.Message) => {
 			let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
 			if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
 				message.channel.send('You do not have the required permissions.');
-				return;
+				return
 			}
 			eval(out);
-			message.channel.send('Completed \n')
+			message.channel.send('Completed \n ')
 		}
 	} catch (error) {
 		console.log(error)
