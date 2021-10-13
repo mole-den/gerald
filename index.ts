f566c39b0dd56c5e14b3ee5fa3101d441d518b25
ofoxsmith <77560533+ofoxsmith@users.noreply.github.com>
2021-10-13 15:21:26 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const prefix = "g";
 //const guildID = '576344535622483968';
 
 const token: string = 'NjcxMTU2MTMwNDgzMDExNjA1.Xi402g.Qt5Ueo_U5m87MLtcnXnM_3xx0yo'; //the sacred texts!
-const dbToken: string = `postgres://eswctjqvpzzbof:b4d93101ae7dcbaadcc4f72e791f5784b6001d2cbd17a8e7378939bd2feffc33@ec2-44-199-86-61.compute-1.amazonaws.com:5432/dfmuinj2u5v6db`;
+const dbToken: string = `postgres://wctjqvpzzbof:b4d93101ae7dcbaadcc4f72e791f5784b6001d2cbd17a8e7378939bd2feffc33@ec2-44-199-86-61.compute-1.amazonaws.com:5432/dfmuinj2u5v6db`;
 console.log(process.version);
 
 bot.on('ready', () => {
