2906aa0c2ca1639a796e4f0c4c70dc902326c288
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-10-20 08:50:16 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -243,7 +243,7 @@ bot.on('messageCreate', async (message: discord.Message) => {
 					let member= await msg.guild?.members.fetch(msg);
 					if (!member) return;
 					let name = (member.nickname) ? member.nickname : `${msg.author.username + msg.author.discriminator}`
-					message.channel.send(`Message from ${name} \n ${message.content}`)
+					message.channel.send(`Message from ${name} \n ${msg.content}`)
 				})
 			}
 		}
