b139b35469153d4b3f531614a9b616e965c1a948
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-10-20 14:40:57 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -77,7 +77,7 @@ bot.on('messageCreate', (message: discord.Message) => {
 		(channel as discord.TextChannel).send(`**${message.author.tag}** said: \`${message.content}\` in ${message.guild!.name}`);
 	}
 });
-let messageDeleted : discord.Message;
+let messageDeleted: discord.Message;
 let deletedTime: Date
 bot.on('messageDelete', async (message) => {
 	messageDeleted = (message as discord.Message)
@@ -263,6 +263,85 @@ bot.on('messageCreate', async (message: discord.Message) => {
 			let timeString = moment(deletedTime).clone().tz("Australia/Sydney").format('llll');
 			let name = (member?.nickname) ? member.nickname : `${msg.author.username}#${msg.author.discriminator}`;
 			await message.channel.send(`**Deleted Message from ${name}**: *${timeString}*\n ${msg.content}`)
+		} else if (command === 'amogus') {
+			message.channel.send(`
+Among Us in real life (sus, sus)
+Among Us in real life (sus, sus)
+Playing Among Us in real life, spaceship with my crew
+Gonna split up, spread out 'cause we all got tasks to do
+Gotta find the imposter as they try to sabotage
+Who can we trust in this Among Us entourage?
+Heard a sound, turned around, looking up, looking down
+Then I find a dead body
+Gotta blow the horn, emergency discussion, who should we believe?
+Can't decide, so, now we leave, not sure what I'm about to see
+Will it be a dead body? Still have a task I must complete
+Who's the impostеr?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can wе trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+I'm a ghost, I'm my biggest fear, got killed by the imposter
+I was playing in the game, but now I'm just a watcher
+I'm paying attention using my 360 vision
+If it gets quiet, I listen
+Make sure there's nothing I'm missing
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+It wasn't me (or me)
+It definitely wasn't me
+No one can be trusted because someone is lying
+We still have tasks to do, so, let's split up so we can win
+Everyone is sus so let this last round begin
+There's an imposter among us
+Trying to take something from us
+We're still over here working
+While she's just ghosting above us
+Someone's under suspicion
+Sabotaging our mission
+Doing my task in the kitchen
+Until I stop, look, and listen
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+`)
+			message.channel.send(`
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+Who's the imposter?
+Looking sus, who can we trust?
+I'm the I-M-P-O-S-T-E-R, that's me
+While everyone's doing tasks, I'm going through vents on a killing spree
+Yeah, I'm sus in Among Us
+We can discuss whoever took your life
+But if you think you're running this game, oh-yeah
+That's me holding this knife
+I'm the imposter
+Looking sus, who can we trust?
+I'm the imposter
+Looking sus, who can we trust?
+I'm the imposter
+Looking sus, who can we trust?
+I'm the imposter
+`)
 		}
 	} catch (error) {
 		console.log("error");
