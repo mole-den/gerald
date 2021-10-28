1f2960273791bb2adfe5631800956ff8a59ae165
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-10-28 12:28:09 +1100

    Added blacklist command

--- index.ts
+++ index.ts
@@ -53,10 +53,13 @@ const db = new pg.Client({
 bot.login(token);
 //egg
 
-bot.on('guildMemberAdd', (member) => {
-	member;
+bot.on('guildMemberAdd', async (member) => {
 	db.query(`INSERT INTO gmember (guild, userid) VALUES ($1, $2) ON CONFLICT DO NOTHING`,
-		[BigInt(member.guild.id), BigInt(member.id)])
+		[BigInt(member.guild.id), BigInt(member.id)]);
+	let x = await db.query(`SELECT * FROM gmember WHERE guild = $1 AND userid = $2 AND blacklisted`, [BigInt(member.guild.id), BigInt(member.id)]);
+	if (x.rows.length > 0) {
+		member.ban({ reason: 'Blacklisted' });
+	}
 })
 
 bot.on('userUpdate', async (user) => {
@@ -207,81 +210,117 @@ bot.on('messageCreate', async (message: discord.Message) => {
 			message.channel.send(`You are not as cool as me.`);
 		} else if (command === `invite`) {
 			message.channel.send(`https://discord.com/oauth2/authorize?client_id=671156130483011605&scope=bot&permissions=8`);
-		} else if (command === 'smite') { /*
+		} else if (command === 'smite') {
 			if (message.channel.type === 'DM') return;
 			if (message.guild === null) return;
-			if (message.member!.permissions.has(discord.Permissions.FLAGS.BAN_MEMBERS, true)) {
-				// if args[1] = 'add' then update the database by adding the mentioned users id to the blacklisted users in the database
-				if (args[1] === 'add') {
+			if (message.member!.permissions.has(discord.Permissions.FLAGS.BAN_MEMBERS)) {
+				// if args[0] = 'add' then update the database by adding the mentioned users id to the blacklisted users in the database
+				if (args[0] === 'add') {
 					let user = message.mentions.members?.first();
 					if (user) {
-						await db.query('INSERT INTO gmember (userid, blacklisted, guild) VALUES ($1, $2, $3) ON CONFLICT UPDATE', [user.id, true, message.guild.id]);
+						await db.query(`INSERT INTO gmember (userid, guild, blacklisted) VALUES ($1, $2, $3) 
+						ON CONFLICT (userid, guild) DO UPDATE SET blacklisted = $3`, [user.id, message.guild.id, true]);
+						message.guild.bans.create(user, {reason: 'Blacklisted', days: 0});
 						message.channel.send(`${user.user.username} has been added to the blacklist`);
 					} else {
-						message.channel.send(`Please mention a user to add to the blacklist`);
+						let num = parseInt(args[1]);
+						if (num !== NaN) {
+							await db.query('INSERT INTO gmember (userid, blacklisted, guild) VALUES ($1, $2, $3) ON CONFLICT DO UPDATE SET blacklisted = $2', [num, true, message.guild.id]);
+							message.channel.send(`${num} has been added to the blacklist`);
+						};
 					};
-				// if args[1] = 'remove' then update the database by removing the mentioned users id from the blacklisted users in the database
-				} else if (args[1] === 'remove') {
+					// if args[0] = 'remove' then update the database by removing the mentioned users id from the blacklisted users in the database
+				} else if (args[0] === 'remove') {
 					let user = message.mentions.members?.first();
 					if (user) {
 						db.query('UPDATE gmember SET blacklisted = false WHERE userid = $1 AND guild = $2', [user.id, message.guild.id]);
 						message.channel.send(`${user.user.username} has been removed from the blacklist`);
+						message.guild.members.unban(user);
 					} else {
-						message.channel.send(`Please mention a user to remove from the blacklist`);
+						let num = parseInt(args[1]);
+						if (num === NaN) return;
+						let q = await db.query('UPDATE gmember SET blacklisted = false WHERE userid = $1 AND guild = $2', [num, message.guild.id]);
+						if (q.rowCount === 0) return;
+						message.channel.send(`${num} has been removed from the blacklist`);
+						message.guild.members.unban(num.toString());
 					};
-				// if args[1] = 'list' then send a message containing all the blacklisted users in the database
-				} else if (args[1] === 'list') {
+					// if args[0] = 'list' then send a message containing all the blacklisted users in the database
+				} else if (args[0] === 'list') {
 					let smite = await db.query('SELECT * FROM gmember WHERE blacklisted AND guild = $1', [message.guild.id]);
+					if (smite.rowCount === 0) message.channel.send(`No users are blacklisted`);
 					smite.rows.forEach((i) => {
 						message.channel.send(`${i.userid} is blacklisted`);
 					});
-				// if args[1] = 'clear' then clear the database of all blacklisted users
-				} else if (args[1] === 'clear') {
+					// if args[0] = 'clear' then clear the database of all blacklisted users
+				} else if (args[0] === 'clear') {
+					let banned = await db.query('SELECT * FROM gmember WHERE blacklisted AND guild = $1', [message.guild.id]);
 					await db.query('UPDATE gmember SET blacklisted = false WHERE blacklisted AND guild = $1', [message.guild.id]);
 					message.channel.send(`The blacklist has been cleared`);
-				} else {
-					message.channel.send('You are not allowed to use this command.');
+					banned.rows.forEach((i) => {
+						message.guild!.members.unban(i.userid);
+					});				
 				}
-			} */
-		} else if (command === 'uptime') {
-			let totalSeconds = Math.round(process.uptime())
-			let hours = Math.floor(totalSeconds / 3600);
-			totalSeconds %= 3600;
-			let minutes = Math.floor(totalSeconds / 60);
-			let seconds = totalSeconds % 60;
-			message.channel.send(`${hours} hours, ${minutes} mins, ${seconds} seconds`)
-		} else if (command === 'query') {
-			let str = message.content;
-			let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
-			if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
-				message.channel.send('You do not have the required permissions');
-				return;
-			}
-			console.log(out);
-			let data = await db.query(out)
-			console.log('done');
-			let JSONdata = JSON.stringify(data.rows, null, 1);
-			if (JSONdata?.length && JSONdata.length < 2000) {
-				message.channel.send(`${data.command} completed - ${data.rowCount} rows, \n${JSONdata}`);
-				return;
-			} else if (JSONdata?.length && JSONdata.length > 2000) {
-				const buffer = Buffer.from(JSONdata)
-				const attachment = new discord.MessageAttachment(buffer, 'file.json');
-				message.channel.send(`${data.command} completed - ${data.rowCount} rows,`);
-				message.channel.send({ files: [attachment] });
 			} else {
-				message.channel.send(`${data.command} completed - ${data.rowCount} rows,`);
+				message.channel.send('You are not allowed to use this command.');
+			}
+	} else if (command === 'uptime') {
+		let totalSeconds = Math.round(process.uptime())
+		let hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = totalSeconds % 60;
+		message.channel.send(`${hours} hours, ${minutes} mins, ${seconds} seconds`)
+	} else if (command === 'query') {
+		let str = message.content;
+		let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
+		if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
+			message.channel.send('You do not have the required permissions');
+			return;
+		}
+		console.log(out);
+		let data = await db.query(out)
+		console.log('done');
+		let JSONdata = JSON.stringify(data.rows, null, 1);
+		if (JSONdata?.length && JSONdata.length < 2000) {
+			message.channel.send(`${data.command} completed - ${data.rowCount} rows, \n${JSONdata}`);
+			return;
+		} else if (JSONdata?.length && JSONdata.length > 2000) {
+			const buffer = Buffer.from(JSONdata)
+			const attachment = new discord.MessageAttachment(buffer, 'file.json');
+			message.channel.send(`${data.command} completed - ${data.rowCount} rows,`);
+			message.channel.send({ files: [attachment] });
+		} else {
+			message.channel.send(`${data.command} completed - ${data.rowCount} rows,`);
+		}
+
+	} else if (command === "eval") {
+		let str = message.content;
+		let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
+		if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
+			message.channel.send('You do not have the required permissions.');
+			return;
+		}
+		try {
+			eval(out);
+		} catch (error) {
+			console.log("error");
+			console.log(error);
+			if (message.author.id == "471907923056918528" || message.author.id == "811413512743813181") {
+				lastChannel.send(`Unhandled exception: \n ${error}`);
+				return;
 			}
 
-		} else if (command === "eval") {
-			let str = message.content;
-			let out = str.substring(str.indexOf('```') + 3, str.lastIndexOf('```'));
-			if (message.author.id !== "471907923056918528" && message.author.id !== "811413512743813181") {
-				message.channel.send('You do not have the required permissions.');
-				return;
-			}
+			lastChannel.send(`Unhandled exception: \n ${error}`);
+		}
+		message.channel.send('Completed \n');
+	} else if (command === "db-setup") {
+		if (message.author.id !== '811413512743813181') return;
+		if (!message.guild) return
+		let users = await message.guild.members.fetch();
+		users?.forEach((i) => {
 			try {
-				eval(out);
+				db.query('INSERT INTO gmember(guild, userid, username) VALUES ($1, $2, $3)',
+					[BigInt(i.guild.id), BigInt(i.user.id), `${i.user.username}#${i.user.discriminator}`])
 			} catch (error) {
 				console.log("error");
 				console.log(error);
@@ -289,112 +328,93 @@ bot.on('messageCreate', async (message: discord.Message) => {
 					lastChannel.send(`Unhandled exception: \n ${error}`);
 					return;
 				}
-
 				lastChannel.send(`Unhandled exception: \n ${error}`);
 			}
-			message.channel.send('Completed \n');
-		} else if (command === "db-setup") {
-			if (message.author.id !== '811413512743813181') return;
-			if (!message.guild) return
-			let users = await message.guild.members.fetch();
-			users?.forEach((i) => {
-				try {
-					db.query('INSERT INTO gmember(guild, userid, username) VALUES ($1, $2, $3)',
-						[BigInt(i.guild.id), BigInt(i.user.id), `${i.user.username}#${i.user.discriminator}`])
-				} catch (error) {
-					console.log("error");
-					console.log(error);
-					if (message.author.id == "471907923056918528" || message.author.id == "811413512743813181") {
-						lastChannel.send(`Unhandled exception: \n ${error}`);
-						return;
-					}
-					lastChannel.send(`Unhandled exception: \n ${error}`);
-				}
-			});
-		} else if (command === 'status') {
-			/*
-			let user = message.mentions.users.first()
-			if (user) {
-				let member = await message.guild?.members.fetch(user);
-				if (member && member.presence) {
-					let presence = member.presence.activities.filter(x => x.type === "PLAYING");
-					let x = "";
-					if (presence[0]) x = `Playing **${presence[0].name}**`;
-					let status = (member.id !== "536047005085204480") ? member.presence.status : "cringe";
-					message.channel.send(`${(member.nickname || user.username)} is ${status}\n${x}`);
-				}
-			} */
-		} else if (command === 'ping') {
-			message.channel.send(`Websocket heartbeat: ${bot.ws.ping}ms`)
-		} else if (command === 'gay') {
-			if (args[0] === 'add') {
-				if (args[1] === undefined) return;
+		});
+	} else if (command === 'status') {
+		/*
+		let user = message.mentions.users.first()
+		if (user) {
+			let member = await message.guild?.members.fetch(user);
+			if (member && member.presence) {
+				let presence = member.presence.activities.filter(x => x.type === "PLAYING");
+				let x = "";
+				if (presence[0]) x = `Playing **${presence[0].name}**`;
+				let status = (member.id !== "536047005085204480") ? member.presence.status : "cringe";
+				message.channel.send(`${(member.nickname || user.username)} is ${status}\n${x}`);
+			}
+		} */
+	} else if (command === 'ping') {
+		message.channel.send(`Websocket heartbeat: ${bot.ws.ping}ms`)
+	} else if (command === 'gay') {
+		if (args[0] === 'add') {
+			if (args[1] === undefined) return;
+			await db.query('UPDATE gmember SET sexuality=$1 WHERE userid = $2',
+				[args[1], message.author.id]);
+			message.channel.send(`set ${message.author.username} to ${args[1]}`);
+			return;
+		} else if (args[0] === 'alter') {
+			let mem = message.mentions.members?.first()
+			if (!mem) return;
+			if (message.member?.permissions.has(discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
 				await db.query('UPDATE gmember SET sexuality=$1 WHERE userid = $2',
-					[args[1], message.author.id]);
-				message.channel.send(`set ${message.author.username} to ${args[1]}`);
-				return;
-			} else if (args[0] === 'alter') {
-				let mem = message.mentions.members?.first()
-				if (!mem) return;
-				if (message.member?.permissions.has(discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
-					await db.query('UPDATE gmember SET sexuality=$1 WHERE userid = $2',
-						[args[2], mem.id]);
-					message.channel.send(`set ${mem.user.username} to ${args[2]}`);
-				};
-				return;
-			}
-			message.mentions.members?.each(async (eachmem) => {
-				let s = await db.query('SELECT * FROM gmember WHERE userid = $1', [BigInt(eachmem.id)])
-				message.channel.send(`${(eachmem.nickname !== null) ? eachmem.nickname : eachmem.user.username} is ${s.rows[0].sexuality}`);
-			})
-			message.mentions.roles?.each(async (eachrole) => {
-				eachrole.members.each(async (eachmem) => {
-					let s = await db.query('SELECT * FROM gmember WHERE userid = $1', [BigInt(eachmem.id)])
-					message.channel.send(`${(eachmem.nickname !== null) ? eachmem.nickname : eachmem.user.username} is ${s.rows[0].sexuality}`)
-				})
-			})
-		} else if (command === 'dump') {
-			let id = message.mentions.channels.first()?.id;
-			if (id) {
-				let channel = await message.guild?.channels.fetch(id);
-				if (!channel || channel.type === 'GUILD_STAGE_VOICE' || channel.type === 'GUILD_VOICE'
-					|| channel.type === 'GUILD_CATEGORY' || channel.type === 'GUILD_STORE') return;
-				let lim = parseInt(args[1]);
-				if (lim === NaN) return;
-				let messages = await channel.messages.fetch({ limit: lim });
-				messages = messages.filter(msg => (msg.author.bot === false || msg.system === false));
-				let messagesArray = Array.from(messages.values()).reverse();
-				messagesArray.forEach(async msg => {
-					let member = await msg.guild?.members.fetch(msg);
-					if (!member) return;
-					let timestamp = lux.DateTime.fromJSDate(msg.createdAt);
-					let timeString = timestamp.setZone("Australia/Sydney").toFormat('FFFF');
-					let name = (member.nickname) ? member.nickname : `${msg.author.username}#${msg.author.discriminator}`;
-					await message.channel.send(`**Message from ${name}**: *${timeString}*\n ${msg.content}`)
-				})
-			}
-		} else if (command === 'deleted') {
-			if (!message.guildId) return
-			let del = await db.query('SELECT * FROM deletedmsg WHERE guild=$2 ORDER BY updated_at DESC LIMIT $1;',
-				[(args[0]) ? Number((args[0])) : 10, message.guildId]);
-			del.rows.forEach(async (msg) => {
-				let member = await message.guild?.members.fetch(msg.author);
-				if (!member) return;
-				let timeString = lux.DateTime.fromSeconds(msg.timestamp).setZone("Australia/Sydney").toFormat('tt DD');
-				let name = (member?.nickname) ? member.nickname : `${member?.user.username}#${member.user.discriminator}`;
-				let cnl = await (await bot.guilds.fetch(message.guildId!.toString())).channels.fetch(msg.channel) as discord.TextChannel;
-				await message.channel.send(`**Deleted Message from ${name} in <#${msg.channel}>**: *${timeString}*\n ${discord.Util.cleanContent(msg.content, cnl)}`)
-			})
-		}
-	} catch (error) {
-		console.log("error");
-		console.log(error);
-		if (message.author.id == "471907923056918528" || message.author.id == "811413512743813181") {
-			lastChannel.send(`Unhandled exception: \n ${error}`);
+					[args[2], mem.id]);
+				message.channel.send(`set ${mem.user.username} to ${args[2]}`);
+			};
 			return;
 		}
-		lastChannel.send(`Unhandled exception: \n ${error}`);
+		message.mentions.members?.each(async (eachmem) => {
+			let s = await db.query('SELECT * FROM gmember WHERE userid = $1', [BigInt(eachmem.id)])
+			message.channel.send(`${(eachmem.nickname !== null) ? eachmem.nickname : eachmem.user.username} is ${s.rows[0].sexuality}`);
+		})
+		message.mentions.roles?.each(async (eachrole) => {
+			eachrole.members.each(async (eachmem) => {
+				let s = await db.query('SELECT * FROM gmember WHERE userid = $1', [BigInt(eachmem.id)])
+				message.channel.send(`${(eachmem.nickname !== null) ? eachmem.nickname : eachmem.user.username} is ${s.rows[0].sexuality}`)
+			})
+		})
+	} else if (command === 'dump') {
+		let id = message.mentions.channels.first()?.id;
+		if (id) {
+			let channel = await message.guild?.channels.fetch(id);
+			if (!channel || channel.type === 'GUILD_STAGE_VOICE' || channel.type === 'GUILD_VOICE'
+				|| channel.type === 'GUILD_CATEGORY' || channel.type === 'GUILD_STORE') return;
+			let lim = parseInt(args[1]);
+			if (lim === NaN) return;
+			let messages = await channel.messages.fetch({ limit: lim });
+			messages = messages.filter(msg => (msg.author.bot === false || msg.system === false));
+			let messagesArray = Array.from(messages.values()).reverse();
+			messagesArray.forEach(async msg => {
+				let member = await msg.guild?.members.fetch(msg);
+				if (!member) return;
+				let timestamp = lux.DateTime.fromJSDate(msg.createdAt);
+				let timeString = timestamp.setZone("Australia/Sydney").toFormat('FFFF');
+				let name = (member.nickname) ? member.nickname : `${msg.author.username}#${msg.author.discriminator}`;
+				await message.channel.send(`**Message from ${name}**: *${timeString}*\n ${msg.content}`)
+			})
+		}
+	} else if (command === 'deleted') {
+		if (!message.guildId) return
+		let del = await db.query('SELECT * FROM deletedmsg WHERE guild=$2 ORDER BY updated_at DESC LIMIT $1;',
+			[(args[0]) ? Number((args[0])) : 10, message.guildId]);
+		del.rows.forEach(async (msg) => {
+			let member = await message.guild?.members.fetch(msg.author);
+			if (!member) return;
+			let timeString = lux.DateTime.fromSeconds(msg.timestamp).setZone("Australia/Sydney").toFormat('tt DD');
+			let name = (member?.nickname) ? member.nickname : `${member?.user.username}#${member.user.discriminator}`;
+			let cnl = await (await bot.guilds.fetch(message.guildId!.toString())).channels.fetch(msg.channel) as discord.TextChannel;
+			await message.channel.send(`**Deleted Message from ${name} in <#${msg.channel}>**: *${timeString}*\n ${discord.Util.cleanContent(msg.content, cnl)}`)
+		})
 	}
+} catch (error) {
+	console.log("error");
+	console.log(error);
+	if (message.author.id == "471907923056918528" || message.author.id == "811413512743813181") {
+		lastChannel.send(`Unhandled exception: \n ${error}`);
+		return;
+	}
+	lastChannel.send(`Unhandled exception: \n ${error}`);
+}
 });
 
 
