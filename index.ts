d1d5acc92bc30df636ef0392bd7130a25bbe58b1
ofoxsmith <77560533+ofoxsmith@users.noreply.github.com>
2021-11-07 16:24:58 +1100

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ process.on('uncaughtException', async error => {
 	if (!bot) { process.exit() }
 	let x = await (await bot.guilds.fetch('895064783135592449')).channels.fetch('903400898397622283') as discord.TextChannel;
 	x.send(` <@!811413512743813181> <@!471907923056918528>\n Unhandled exception: \n ${error}`);
-	process.exit()
+	process.exit();
 });
 process.on('unhandledRejection', async error => {
 	console.log(error);
