cb3b4355571bdb2b18606f2bcb9dee3b081cc6d4
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-11-07 16:47:42 +1100

    Updated init.sql

--- init.sql
+++ init.sql
@@ -1,47 +1,51 @@
-DROP TABLE guild IF EXISTS;
-DROP TABLE gmember IF EXISTS;
-DROP TABLE deletedmsg IF EXISTS;
-DROP FUNCTION replace_oldest IF EXISTS;
-
-CREATE TABLE guild (
+CREATE TABLE guilds (
     id INT GENERATED ALWAYS AS IDENTITY,
     guildid BIGINT UNIQUE NOT NULL
 );
 
-CREATE TABLE gmember (
+CREATE TABLE members (
     id INT GENERATED ALWAYS AS IDENTITY,
-    guild BIGINT REFERENCES guild(guildid),
+    guild BIGINT REFERENCES guilds(guildid),
     blacklisted BOOL DEFAULT FALSE,
     userid BIGINT NOT NULL,
     sexuality VARCHAR(30) DEFAULT 'straight',
-    username VARCHAR(37) NOT NULL,
+    username VARCHAR(32) NOT NULL,
+    fullname DEFAULT CONCAT(username, '#', userid),
+    discrim CHAR(4) NOT NULL,
     UNIQUE(guild, userid)
 );
 
-CREATE TABLE deletedmsg (
+CREATE TABLE deletedmsgs (
     id INT GENERATED ALWAYS AS IDENTITY,
     author BIGINT,
     content TEXT,
     guildid BIGINT,
-    timestamp INT NOT NULL,
+    msgtime INT NOT NULL,
     channel BIGINT NOT NULL,
     deleted_time INT NOT NULL,
-    FOREIGN KEY (author, guildid) REFERENCES gmember(userid, guild)
+    FOREIGN KEY (author, guildid) REFERENCES members(userid, guild)
+);
+
+CREATE TABLE punishments (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    member BIGINT REFERENCES members(userid),
+    type VARCHAR(5) NOT NULL,
+    reason VARCHAR(255) DEFAULT 'not given',
+    created_time TIMESTAMP NOT NULL,
+    duration INT DEFAULT 0,
 );
 
 CREATE OR REPLACE FUNCTION replace_oldest() RETURNS TRIGGER AS $$
 BEGIN
-	IF (SELECT count(*) FROM deletedmsg WHERE guildid = NEW.guildid) > 100 THEN
-	DELETE FROM deletedmsg
-	WHERE id IN (
-   		SELECT id FROM
-   		deletedmsg ORDER BY timestamp LIMIT 1);
+	IF count(SELECT * FROM deletedmsg WHERE guildid = NEW.guildid) > 100 THEN
+	    DELETE FROM deletedmsg
+	    WHERE id IN (SELECT id FROM deletedmsg ORDER BY timestamp LIMIT 1);
 	END IF;
 END;
 $$ 
 LANGUAGE plpgsql;
 
 CREATE TRIGGER cycle_oldest
-    AFTER UPDATE ON deletedmsg 
+    AFTER UPDATE ON deletedmsgs 
     FOR EACH ROW 
     EXECUTE PROCEDURE replace_oldest();
