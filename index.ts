e735fb7af2713600cfaabdd478a2c04ef00d8266
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-11-09 15:20:04 +1100

    Updated regex

--- index.ts
+++ index.ts
@@ -61,11 +61,11 @@ function getRandomArbitrary(min: number, max: number) {
 	return Math.round(Math.random() * (max - min) + min);
 };
 
-function durationToMS(duration: string): number {
-	let timeRegex = / ([0-9]+( +|)(m |min |mins |minute |minutes |h |hr |hrs |hour |hours |d |day |days |wk |wks |week |weeks |mth |mths |month |months |y |yr |yrs |year |years ))+/gmi
+function durationToMS(duration: string): number | null {
+	let timeRegex = /([0-9]+( +|)(m($| )|min($| )|mins($| )|minute($| )|minutes($| )|h($| )|hr($| )|hrs($| )|hour($| )|hours($| )|d($| )|day($| )|days($| )|wk($| )|wks($| )|week($| )|weeks($| )|mth($| )|mths($| )|month($| )|months($| )|y($| )|yr($| )|yrs($| )|year($| )|years($| )))+/gmi
 	let durationMS = 0;
 	let durationArr = duration.match(timeRegex);
-	if (!durationArr) return 0;
+	if (!durationArr) return null;
 	durationArr.forEach((d) => {
 		let time = d.match(/[0-9]+/gmi);
 		let unit = d.match(/[a-zA-Z]+/gmi);
