222240df3b424d5db077afc3c28d1a1c9146ad88
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-11-10 11:27:16 +1100

    Add sapphire

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import * as pg from 'pg';
 import * as lux from 'luxon';
 import axios from 'axios';
 import cron from 'node-cron';
+import * as sapphire from '@sapphire/framework';
+import { durationToMS } from './functions'
 voice;
 cron;
 process.on('uncaughtException', async (error) => {
@@ -27,7 +29,10 @@ const myIntents = new discord.Intents();
 myIntents.add(discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_MESSAGES,
 	discord.Intents.FLAGS.DIRECT_MESSAGES, discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
 	discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, discord.Intents.FLAGS.GUILD_PRESENCES);
-const bot = new discord.Client({ intents: myIntents });
+const bot = new sapphire.SapphireClient({ 
+	intents: myIntents,
+	defaultPrefix: 'g',
+});
 const logmessages = false;
 const prefix = "g";
 const token = <string>process.env.TOKEN
@@ -61,61 +66,6 @@ function getRandomArbitrary(min: number, max: number) {
 	return Math.round(Math.random() * (max - min) + min);
 };
 
-function durationToMS(duration: string): number | null {
-	let timeRegex = /([0-9]+( +|)(m($| )|min($| )|mins($| )|minute($| )|minutes($| )|h($| )|hr($| )|hrs($| )|hour($| )|hours($| )|d($| )|day($| )|days($| )|wk($| )|wks($| )|week($| )|weeks($| )|mth($| )|mths($| )|month($| )|months($| )|y($| )|yr($| )|yrs($| )|year($| )|years($| )))+/gmi
-	let durationMS = 0;
-	let durationArr = duration.match(timeRegex);
-	if (!durationArr) return null;
-	durationArr.forEach((d) => {
-		let time = d.match(/[0-9]+/gmi);
-		let unit = d.match(/[a-zA-Z]+/gmi);
-		if (!time || !unit) return;
-		let timeNum = parseInt(time[0]);
-		let unitNum = 0;
-		switch (unit[0].toLowerCase()) {
-			case 'm':
-			case 'min':
-			case 'mins':
-			case 'minute':
-			case 'minutes':
-				unitNum = 60000;
-				break;
-			case 'h':
-			case 'hr':
-			case 'hrs':
-			case 'hour':
-			case 'hours':
-				unitNum = 3600000;
-				break;
-			case 'd':
-			case 'day':
-			case 'days':
-				unitNum = 86400000;
-				break;
-			case 'wk':
-			case 'wks':
-			case 'week':
-			case 'weeks':
-				unitNum = 604800000;
-				break;
-			case 'mth':
-			case 'mths':
-			case 'month':
-			case 'months':
-				unitNum = 2592000000;
-				break;
-			case 'y':
-			case 'yr':
-			case 'yrs':
-			case 'year':
-			case 'years':
-				unitNum = 31536000000;
-				break;
-		}
-		durationMS += timeNum * unitNum;
-	})
-	return durationMS;
-}
 
 db.connect();
 void bot.login(token);
