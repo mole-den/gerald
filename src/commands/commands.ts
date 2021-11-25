48f47b39001bd4055fd4f9c9376293f4e7e9db09
Oliver Fox-Smith <77560533+ofoxsmith@users.noreply.github.com>
2021-11-25 19:12:32 +1100

    Update commands.ts

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -319,7 +319,7 @@ export class dieCommand extends sapphire.Command {
 })
 export class amogusCommand extends sapphire.Command {
     public async messageRun(message: discord.Message) {
-	amongus = Math.random(100);
+	amongus = getRandomArbitrary(0, 100)
         if(amongus == 1){
 		message.channel.send('<@!809675885330432051>');	
 		amongus = 0;
