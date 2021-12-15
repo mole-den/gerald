78d7862c5e3a4dd4e48c630c838d32b49b5917bb
Oliver Fox-Smith <ofoxsmith@outlook.com>
2021-12-16 10:54:27 +1100

    add schema dump

--- init.sql
+++ init.sql
@@ -1,55 +1,58 @@
+CREATE FUNCTION replace_oldest() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+IF (SELECT count(*) FROM deletedmsgs WHERE guildid = NEW.guildid) > 100 THEN
+	    DELETE FROM deletedmsgs
+	    WHERE id IN (SELECT id FROM deletedmsgs WHERE guildid = NEW.guildid ORDER BY timestamp LIMIT 1);
+END IF;
+END;
+$$;
+
 CREATE TABLE guilds (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    guildid BIGINT UNIQUE NOT NULL,
-    joined_at TIMESTAMP NOT NULL
+    id integer GENERATED ALWAYS AS IDENTITY,
+    guildid bigint NOT NULL,
+    joined_at timestamp without time zone NOT NULL,
+    prefix text DEFAULT 'g'::character varying,
+    disabled character varying(255)[] DEFAULT '{}'::character varying[],
+    delmsg_public_key text,
+    UNIQUE (guildid)
 );
 
+
 CREATE TABLE members (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    guild BIGINT REFERENCES guilds(guildid),
-    blacklisted BOOL DEFAULT FALSE,
-    userid BIGINT NOT NULL,
-    sexuality VARCHAR(30) DEFAULT 'straight',
-    username VARCHAR(37),
-    UNIQUE(guild, userid)
+    id integer GENERATED ALWAYS AS IDENTITY,
+    guild bigint,
+    userid bigint NOT NULL,
+    sexuality text DEFAULT 'straight'::text,
+    UNIQUE (guild, userid),
+    FOREIGN KEY (guild) REFERENCES guilds(guildid)
 );
 
 CREATE TABLE deletedmsgs (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    author BIGINT,
-    content TEXT,
-    guildid BIGINT,
-    msgtime TIMESTAMP NOT NULL,
-    channel BIGINT NOT NULL,
-    deleted_time TIMESTAMP NOT NULL,
-    deleted_by VARCHAR(37) NOT NULL,
-    msgid BIGINT UNIQUE,
+    id integer GENERATED ALWAYS AS IDENTITY,
+    author bigint,
+    content text,
+    guildid bigint,
+    msgtime timestamp without time zone NOT NULL,
+    channel bigint NOT NULL,
+    deleted_time timestamp without time zone NOT NULL,
+    deleted_by character varying(37) NOT NULL,
+    msgid bigint,
+    attachments jsonb,
+	UNIQUE (msgid),
     FOREIGN KEY (author, guildid) REFERENCES members(userid, guild)
 );
 
 CREATE TABLE punishments (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    member BIGINT,
-    guild BIGINT,
-    type VARCHAR(5) NOT NULL,
-    reason VARCHAR(255) DEFAULT 'not given',
-    created_time TIMESTAMP NOT NULL,
-    duration INT DEFAULT 0,
-    resolved BOOLEAN DEFAULT FALSE,
+    id integer GENERATED ALWAYS AS IDENTITY,
+    member bigint,
+    guild bigint,
+    type character varying(5) NOT NULL,
+    reason character varying(255) DEFAULT 'not given'::character varying,
+    created_time timestamp without time zone NOT NULL,
+    resolved boolean DEFAULT false,
+    ends_at timestamp without time zone,
+    ends timestamp without time zone,
     FOREIGN KEY (member, guild) REFERENCES members(userid, guild)
 );
-
-CREATE OR REPLACE FUNCTION replace_oldest() RETURNS TRIGGER AS $$
-BEGIN
-    IF (SELECT count(*) FROM deletedmsg WHERE guildid = NEW.guildid) > 100 THEN
-	    DELETE FROM deletedmsg
-	    WHERE id IN (SELECT id FROM deletedmsg WHERE guildid = NEW.guildid ORDER BY timestamp LIMIT 1);
-    END IF;
-END;
-$$ 
-LANGUAGE plpgsql;
-
-CREATE TRIGGER cycle_oldest
-    AFTER UPDATE ON deletedmsgs 
-    FOR EACH ROW 
-    EXECUTE PROCEDURE replace_oldest();
