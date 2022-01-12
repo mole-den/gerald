c3510a7f2234d69e20cf6af723333dbb75dcede2
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-01-13 09:06:21 +1100

    add getTasks method

--- src/taskManager.ts
+++ src/taskManager.ts
@@ -2,7 +2,7 @@ import { EventEmitter } from "events"
 //import * as cron from 'node-cron'
 import * as pg from 'pg'
 const db = new pg.Pool({
-	
+	connectionString: 'postgres://dgwgajohpiooid:d4e3c9d5583ddb1fb429057b86cab5b6e5d28c8669129ea095ff2a899fa9d496@ec2-34-233-214-228.compute-1.amazonaws.com:5432/d282ttnf02pikk',
 	ssl: {
 		rejectUnauthorized: false
 	}
@@ -12,7 +12,7 @@ type validJSON = string | number | boolean | null | { [key: string]: validJSON }
 interface getParams {
 	task?: string,
 	id?: number,
-	time?: string,
+	time?: Date,
 	context?: { [key: string]: validJSON }
 }
 class ScheduledTask {
@@ -47,32 +47,38 @@ class ScheduledTask {
 }
 
 class scheduledTaskManager extends EventEmitter {
-	private id!: number
+	private _id!: number;
+	private get id(): number {
+		return this._id++;
+	}
+	private set id(value: number) {
+		this._id = value;
+	}
 	constructor() {
 		super();
 		this.getID().then(x => this.id = x)
 	}
 	private async getID(): Promise<number> {
 		let x = await db.query('SELECT MAX(id) FROM scheduled_tasks')
-		return x.rows[0].max + 1 ?? 1;
+		return (x.rows[0].max ?? 0) + 1 ?? 1;
 	}
 
 	public newTask(task: { task: string, when: Date, context: { [key: string]: validJSON } }): ScheduledTask {
 		return new ScheduledTask(task, this, this.id)
 	}
 
-	public async getTasks(params: getParams): Promise<null | ScheduledTask | ScheduledTask[]> {
+	public async getTasks(params?: getParams): Promise<null | ScheduledTask | ScheduledTask[]> {
 		let query = 'SELECT * FROM scheduled_tasks'
 		let values: any[] = []
-		if (params.task) {
+		if (params?.task) {
 			query += ' WHERE task = $1'
 			values.push(params.task)
 		}
-		if (params.id) {
+		if (params?.id) {
 			query += ' WHERE id = $1'
 			values.push(params.id)
 		}
-		if (params.time) {
+		if (params?.time) {
 			query += ' WHERE time = $1'
 			values.push(params.time)
 		}
@@ -81,7 +87,7 @@ class scheduledTaskManager extends EventEmitter {
 		if (x.rows.length == 0) {
 			return null
 		}
-		if (params.context !== undefined) {
+		if (params?.context !== undefined) {
 			let keys = Object.keys(params.context)
 			keys.forEach(key => {
 				x.rows = x.rows.filter(y => y.context[key] == params.context![key])
