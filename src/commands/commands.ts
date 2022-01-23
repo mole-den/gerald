bf192977bae3d92124fb45faca5f2c2355b77520
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-01-23 17:50:12 +1100

    e

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -346,7 +346,7 @@ export class infoCommand extends sapphire.Command {
             let embed = new discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Help')
-                .addField('Commands:' , bot.stores.get('commands').map(c => c.name).join('`, `'), true)
+                .addField('Commands:' , bot.stores.get('commands').map(c => c.name).join(','), true)
                 .setFooter({ text: 'Use `help <command>` to get more information on a command' });
             return message.channel.send({
                 embeds: [embed]
