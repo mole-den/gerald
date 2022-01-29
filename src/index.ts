7a38d88bbe2a96717dc6e17a3f11ce6109aaf052
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-01-29 15:26:04 +1100

    allow commands to be case insensitive

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,8 @@ process.on('SIGTERM', async () => {
 
 export const bot = new sapphire.SapphireClient({
 	typing: true,
+	caseInsensitiveCommands: true,
+	caseInsensitivePrefixes: true,
 	intents: new discord.Intents([discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_MESSAGES,
 	discord.Intents.FLAGS.DIRECT_MESSAGES, discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
 	discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS]),
