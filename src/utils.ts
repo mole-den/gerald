8be5a47b0c4bf6e05be09003964a995a18e86632
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-03-28 14:53:14 +1100

    add more functions to utils file

--- src/utils.ts
+++ src/utils.ts
@@ -29,4 +29,37 @@ export namespace utils {
             });
 
     }
+    export async function awaitButtonResponse(interaction: discord.CommandInteraction, response: discord.Message, timeout: number = 15000) {
+        let x: discord.ButtonInteraction<discord.CacheType>;
+        try {
+             x = await response.awaitMessageComponent({
+                filter: (i) => {
+                    if (i.user.id !== interaction.user.id) {
+                        i.reply({
+                            ephemeral: true,
+                            content: `Please stop interacting with the components on this message. They are only for ${interaction.user.toString()}.`,
+                            allowedMentions: { users: [], roles: [] }
+                        })
+                        return false
+                    }
+                    return true
+                }, componentType: "BUTTON", time: timeout
+            })
+        } catch {
+            return null
+        }
+        return x;
+    }
+    export async function disableButtons(response: discord.Message, interaction: discord.CommandInteraction) {
+        let button = response.components;
+        button.forEach(i => {
+            i.components.forEach(x => {
+                x.disabled = true;
+            })
+        })
+        interaction.editReply({
+            components: [...button]
+        })
+    }
+
 }
