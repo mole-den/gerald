c1b1791a366d67c3762bb1d145490d3894d8cf8f
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-03-28 14:54:08 +1100

    add wip command settings class

--- src/commandClass.ts
+++ src/commandClass.ts
@@ -3,8 +3,7 @@ import * as discord from 'discord.js';
 import _ from "lodash";
 import { bugsnag, prisma } from ".";
 
-type optionTypes = "string" | "number" | "discord.Channel" | "discord.Role"
-type settingTypes = string | number | discord.Channel | discord.Role
+type settingTypes = string | number | boolean | Array<settingTypes>
 
 declare module '@sapphire/pieces' {
     interface Container {
@@ -22,7 +21,6 @@ interface Setting {
     id: string
     name: string;
     description: string;
-    type: optionTypes;
     default: settingTypes;
     choices?: settingTypes[] | undefined
 }
@@ -33,13 +31,13 @@ interface SettingWithData extends Setting {
 class SettingsManager {
     readonly settings: Setting[]
     readonly module: string
-    constructor(settings: Setting[], module: string) {
+    settingsHandler: (interaction: discord.CommandInteraction) => Promise<void>
+    constructor(settings: Setting[], module: string, customHandler: (interaction: discord.CommandInteraction) => Promise<void>) {
         this.settings = settings
         this.module = module
+        this.settingsHandler = customHandler
     }
-    public async getSetting(guild: string, settingID: null): Promise<SettingWithData[]>;
-    public async getSetting(guild: string, settingID: string): Promise<SettingWithData>;
-    public async getSetting(guild: string, settingID: string | null = null) {
+    public async getSetting(guild: string): Promise<SettingWithData[]> {
         let x = await prisma.module_settings.findUnique({
             where: {
                 guildid_moduleName: {
@@ -49,65 +47,26 @@ class SettingsManager {
             }
         })
         if (x === null) {
-            let dbSettings = this.settings.map(i => {
-                return { id: i.id, value: i.default }
-            })
-            await prisma.module_settings.create({
+            let a = await prisma.module_settings.create({
                 data: {
                     guildid: guild,
                     moduleName: this.module,
-                    settings: JSON.stringify(dbSettings)
+                    settings: JSON.stringify(this.settings.map(i => {
+                        return <SettingWithData>{
+                            ...i,
+                            value: i.default
+                        }
+                    }))
                 }
             })
-            if (!settingID) return this.settings.map(x => {
-                return this.formatSetting(x)
-            })
-            else {
-                let a = this.settings.find(i => i.id === settingID)
-                if (!a) throw new Error("")
-                return this.formatSetting(a)
-            }
-        } else {
-            let a: {
-                id: string,
-                value: string
-            }[] = <any>x.settings.map(i => {
-                return JSON.parse(i)
-            })
-            if (!settingID) return a.map(x => {
-                return this.dbOutputToSetting(x)
-            })
-            else {
-                let b = a.find(i => i.id === settingID)
-                if (!b) throw new Error("")
-                return this.dbOutputToSetting(b)
-            }
-
+            return JSON.parse(a.settings)
         }
+        return JSON.parse(x.settings)
     }
 
-    public async changeSettings() { }
-    private dbOutputToSetting(x: { id: string, value: string }): SettingWithData {
-        if (!this.settings) throw new Error()
-        let a = this.settings.find(i => i.id === x.id)
-        if (!a) throw new Error();
-        return <SettingWithData>{
-            ...a,
-            value: x.value
-        }
+    public async customHandler(interaction: discord.CommandInteraction) {
+        this.settingsHandler(interaction)
     }
-    private formatSetting(x: Setting): SettingWithData {
-        return {
-            id: x.id,
-            name: x.name,
-            description: x.description,
-            type: x.type,
-            default: x.default,
-            choices: x.choices,
-            value: x.default
-        };
-    }
-
 }
 
 export abstract class Module {
@@ -117,12 +76,15 @@ export abstract class Module {
     constructor(options: ModuleOptions) {
         this.description = options.description;
         this.name = options.name
-        if (options.settings) this.settings = new SettingsManager(options.settings, this.name)
+        if (options.settings) this.settings = new SettingsManager(options.settings, this.name, this.settingsHandler)
         else this.settings = null
     }
 
     abstract load(): Promise<void>
     abstract unload(): Promise<void>
+    public async settingsHandler(interaction: discord.MessageInteraction) {
+        interaction
+    }
 }
 
 export interface geraldCommandOptions extends sapphire.CommandOptions {
@@ -139,12 +101,15 @@ export abstract class GeraldCommand extends sapphire.Command {
         super(context, {
             ...options,
         });
-        if (options.settings) this.settings = new SettingsManager(options.settings, this.name)
+        if (options.settings) this.settings = new SettingsManager(options.settings, this.name, this.settingsHandler)
         else this.settings = null
         if (!options.fullCategory) options.fullCategory = []
         if (options.ownerOnly === true) options.fullCategory.push("_owner")
         if (options.hidden === true) options.fullCategory.push("_hidden")
         if (options.alwaysEnabled === true) options.fullCategory.push("_enabled")
+    }
+    private async settingsHandler(interaction: discord.CommandInteraction) {
+        interaction;
     }
     private messageHandler(error: unknown, message: discord.Message, args: sapphire.Args, context: sapphire.MessageCommand.RunContext): void {
         let channel = message.channel
@@ -191,15 +156,17 @@ export abstract class GeraldCommand extends sapphire.Command {
             })
         }
     }
-    protected slashRun?(interaction: discord.CommandInteraction, context: sapphire.ChatInputCommand.RunContext): sapphire.Awaitable<unknown>
+    protected slashRun?(interaction: discord.CommandInteraction, reply: discord.Message, context: sapphire.ChatInputCommand.RunContext): sapphire.Awaitable<unknown>
     protected chatRun?(message: discord.Message, args: sapphire.Args, context: sapphire.MessageCommand.RunContext): sapphire.Awaitable<unknown>
     protected menuRun?(interaction: discord.ContextMenuInteraction, context: sapphire.ContextMenuCommand.RunContext): sapphire.Awaitable<unknown>
 
-    chatInputRun(interaction: discord.CommandInteraction, context: sapphire.ChatInputCommand.RunContext) {
+    async chatInputRun(interaction: discord.CommandInteraction, context: sapphire.ChatInputCommand.RunContext) {
         if (!this.slashRun) return
+        await interaction.deferReply()
+        let reply = await interaction.fetchReply()
         let x;
         try {
-            x = this.slashRun(interaction, context)
+            x = this.slashRun(interaction, <discord.Message>reply, context)
         } catch (error) {
             this.slashHandler(error, interaction, context)
         }
@@ -237,4 +204,34 @@ export abstract class GeraldCommand extends sapphire.Command {
             this.errorHandler(e, interaction, context)
         })
     }*/
+}
+
+export class CommandManager extends Module {
+    constructor() {
+		super({
+            name: "Command Management",
+            description: "Allows management of commands",
+            settings: [{
+                id: "channelDisabled",
+                name: "channelDisabled",
+                description: "N/A",
+                default: [],
+            }]
+        })
+    }
+    async load(): Promise<void> {}
+    async unload(): Promise<void> {}
+    override async settingsHandler(interaction: discord.CommandInteraction) {
+        let a = (await this.settings!.getSetting(interaction.guildId!)).find(i => i.id === "channelDisabled")
+        console.log(a?.value)
+        let x = new discord.MessageEmbed()
+        x.setTitle("Command management")
+        x.setColor("BLURPLE")
+        .setTimestamp(new Date())
+        ///.addField("Current disabled commands", )
+        interaction.editReply({
+
+        })
+    }
+ 
 }
